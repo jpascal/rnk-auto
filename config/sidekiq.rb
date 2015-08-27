6ad2ec5dd7a6c37961e1b490de3abc9f1bc18d44
Evgeniy Shurmin <eshurmin@gmail.com>
2015-08-27 12:15:26 +0300

    set namespace to 'rnk-auto'

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,5 +1,9 @@
 require_relative 'boot'
 
+Sidekiq.configure_server do |config|
+  config.redis = { namespace: 'rnk-auto' }
+end
+
 Sidekiq::Cron::Job.destroy_all!
 Sidekiq::Cron::Job.new(RNK.config['job'].merge({:'name' => 'RKN::Request', :'klass' => 'RNK::Request'})).save
 
