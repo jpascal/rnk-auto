8d6b91fd578dd246cb6857c63cd9c2d8f979fd08
Evgeniy Shurmin <eshurmin@gmail.com>
2016-06-27 12:41:18 +0300

    missing changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server config(:host), user: config(:user), roles: %w{app}
+server config_yml(:host), user: config_yml(:user), roles: %w{app}
